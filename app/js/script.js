dd5ff56f9e9f9278abeb72c947e820844f885a2f
K.Kishin <riesgraf.birsack.1226@gmail.com>
2021-10-15 15:54:53 +0900

    edit border-color style

--- app/js/script.js
+++ app/js/script.js
@@ -8,7 +8,7 @@ btnSubmit.addEventListener('click', (e) => {
 	const email = document.querySelector('.email');
 	const password = document.querySelector('.password');
 
-	const mailValidate = /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+	const mailRegex = /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
 	e.preventDefault();
 
@@ -16,31 +16,39 @@ btnSubmit.addEventListener('click', (e) => {
 	const errFirstname = document.querySelectorAll('.error-firstname');
 	if (firstName.value === '') {
 		errFirstname.forEach((err) => err.classList.add('validate'));
+		firstName.style.borderColor = 'hsl(0, 100%, 74%)';
 	} else {
 		errFirstname.forEach((err) => err.classList.remove('validate'));
+		firstName.style.borderColor = 'hsl(0, 0%, 87%)';
 	}
 
 	/*============= Last Name =============*/
 	const errLastname = document.querySelectorAll('.error-lastname');
 	if (lastName.value === '') {
 		errLastname.forEach((err) => err.classList.add('validate'));
+		lastName.style.borderColor = 'hsl(0, 100%, 74%)';
 	} else {
 		errLastname.forEach((err) => err.classList.remove('validate'));
+		lastName.style.borderColor = 'hsl(0, 0%, 87%)';
 	}
 
 	/*=============== Email ===============*/
 	const errEmail = document.querySelectorAll('.error-email');
-	if (email.value === '' || !email.value.match(mailValidate)) {
+	if (email.value === '' || !email.value.match(mailRegex)) {
 		errEmail.forEach((err) => err.classList.add('validate'));
+		email.style.borderColor = 'hsl(0, 100%, 74%)';
 	} else {
 		errEmail.forEach((err) => err.classList.remove('validate'));
+		email.style.borderColor = 'hsl(0, 0%, 87%)';
 	}
 
 	/*=============== Password ============*/
 	const errPassword = document.querySelectorAll('.error-password');
 	if (password.value === '') {
 		errPassword.forEach((err) => err.classList.add('validate'));
+		password.style.borderColor = 'hsl(0, 100%, 74%)';
 	} else {
 		errPassword.forEach((err) => err.classList.remove('validate'));
+		password.style.borderColor = 'hsl(0, 0%, 87%)';
 	}
 });
